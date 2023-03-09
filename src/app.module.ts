bf0d87d0e51ea18f0f2e377cfafcd78b5510c486
Brocha05 <brocha_05@hotmail.com>
2023-03-09 11:05:31 -0600

    Using nest g resource to create CRUD of brands

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RootController } from './app.controller';
 import { CarsModule } from './cars/cars.module';
+import { BrandsModule } from './brands/brands.module';
 
 
 @Module({//Módulo principal con sus respectivos módulos hijos y controladores 
-  imports: [CarsModule],//Modulo hijo CarsModule
+  imports: [CarsModule, BrandsModule],//Modulo hijo CarsModule
   controllers: [RootController],//Controlador root de la aplicación
   providers: [],
   exports: [],
